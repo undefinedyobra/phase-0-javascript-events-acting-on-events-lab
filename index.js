901474ee95d3576829cb7cd37402eea619a4b4e4
undefinedyobra <brianmelly008@gmail.com>
2024-04-02 12:43:10 +0300

    events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+const DODGER = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let currentLeft = parseInt(DODGER.style.left || 0, 10);
+  DODGER.style.left = `${currentLeft - 5}px`;
+}
+
+function moveDodgerRight() {
+  let currentLeft = parseInt(DODGER.style.left || 0, 10);
+  DODGER.style.left =`${currentLeft + 5}px`;
+}
